519fa1d7cb2bc40230cdf08c451139b01b4bd24c
Glitch (gblkbt1991) <none>
2021-06-05 14:44:25 +0000

    migrate several mustntcall feature to custom cmd

--- bot/features/mustntcall/teman.js
+++
@@ -1,8 +0,0 @@
-const { isAdmin } = require("./../../utility");
-
-module.exports = (parsed, event) => {
-  return {
-    type: "text",
-    text: isAdmin(event.source.userId) ? "kenaps" : "apaan"
-  };
-};
