24c774a8b08be81fba0d35e6414c9877c1a92851
raf555 <irsoekarno888@gmail.com>
2021-08-20 07:35:01 +0700

    update (#48)
    Co-authored-by: Glitch (gblkbt1991) <none>

--- bot/features/admin/terminal.js
+++ bot/features/admin/terminal.js
@@ -1,6 +1,8 @@
 const { promisify } = require("util");
 const exec = promisify(require("child_process").exec);
 
+/* VERY DANGEROUS FEATURE, USE WITH CAUTION */
+
 module.exports = {
   data: {
     name: "Terminal",
@@ -11,6 +13,10 @@ module.exports = {
     ALIASES: []
   },
   run: async (parsed, event, bot) => {
+    if (event.source.userId !== process.env.admin_id) {
+      throw new Error("Admin only");
+    }
+
     let cmd = parsed.arg;
     const { stdout, stderr } = await exec(cmd);
     return {
