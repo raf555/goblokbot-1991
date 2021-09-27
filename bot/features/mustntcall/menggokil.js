96ee68a5e875fd3ea42b5070118fd314056dce19
Arceister <raynvalshe@gmail.com>
2021-09-27 22:55:40 +0700

    Fix Menggokil

--- bot/features/mustntcall/menggokil.js
+++ bot/features/mustntcall/menggokil.js
@@ -1,20 +1,21 @@
-const db = require("@utils/database");
+// https://img.favpng.com/12/19/12/emoticon-smiley-computer-icons-t-shirt-sunglasses-png-favpng-3vsrdQNcX3TSnVCkVA6pjuMwJ.jpg
 
-module.exports = function(parsed, event, bot) {
-    let msg = parsed.fullMsg
-
-    if (
-        // msg[0].toLowerCase() == "g" &&
-        // msg.length > 2 &&
-        // msg.match(/(r)\1\1+/i)
-        msg.match("menggokil")
-    ) {
-        return {
+module.exports = {
+    data: {
+      name: "Menggokil",
+      description: "Kirim gambar menggokil",
+      usage: "menggokil",
+      CMD: "menggokil",
+      ALIASES: []
+    },
+    run: (parsed, event, bot) => {
+      if (!!parsed.arg) return null;
+      var sendImageMessage = {
         type: "image",
-        originalContentUrl: "https://img.favpng.com/12/19/12/emoticon-smiley-computer-icons-t-shirt-sunglasses-png-favpng-3vsrdQNcX3TSnVCkVA6pjuMwJ.jpg", //"https://i.ibb.co/ChLFsXr/184101.jpg",
-        previewImageUrl: "https://img.favpng.com/12/19/12/emoticon-smiley-computer-icons-t-shirt-sunglasses-png-favpng-3vsrdQNcX3TSnVCkVA6pjuMwJ.jpg",
-        cmd: "menggokil"
-        };
+        originalContentUrl: "https://img.favpng.com/12/19/12/emoticon-smiley-computer-icons-t-shirt-sunglasses-png-favpng-3vsrdQNcX3TSnVCkVA6pjuMwJ.jpg",
+        previewImageUrl: "https://img.favpng.com/12/19/12/emoticon-smiley-computer-icons-t-shirt-sunglasses-png-favpng-3vsrdQNcX3TSnVCkVA6pjuMwJ.jpg"
+      };
+      return [sendImageMessage];
     }
-    // https://img.favpng.com/12/19/12/emoticon-smiley-computer-icons-t-shirt-sunglasses-png-favpng-3vsrdQNcX3TSnVCkVA6pjuMwJ.jpg
-}
+  };
+  
